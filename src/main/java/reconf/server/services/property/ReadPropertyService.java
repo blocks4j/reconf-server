a8a385447aec48f3b3049fe32bdce86c94055265
Danilo Mutti <dmutti@gmail.com>
2014-08-14 18:25:05 -0300

    more code

--- src/main/java/reconf/server/services/property/ReadPropertyService.java
+++ src/main/java/reconf/server/services/property/ReadPropertyService.java
@@ -16,6 +16,7 @@
 package reconf.server.services.property;
 
 import java.util.*;
+import javax.script.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.transaction.annotation.*;
@@ -34,6 +35,7 @@ import com.fasterxml.jackson.databind.*;
 public class ReadPropertyService {
 
     @Autowired PropertyRepository properties;
+    private static final String JS = "var patt = eval(regexp); var result = patt.test(instance);";
     private static ObjectMapper mapper = new ObjectMapper();
 
     @RequestMapping(value="/{prod}/{comp}/{prop}", method=RequestMethod.GET)
@@ -41,28 +43,55 @@ public class ReadPropertyService {
     public ResponseEntity<String> doIt(
             @PathVariable("prod") String product,
             @PathVariable("comp") String component,
-            @PathVariable("prop") String property) {
+            @PathVariable("prop") String property,
+            @RequestParam(value="instance", required=false, defaultValue="unknown") String instance) {
 
         PropertyKey key = new PropertyKey(product, component, property);
         List<String> errors = DomainValidator.checkForErrors(key);
         Property fromRequest = new Property(key);
 
+        HttpHeaders headers = new HttpHeaders();
         if (!errors.isEmpty()) {
-            HttpHeaders headers = getErrorHeader(errors, fromRequest);
+            addErrorHeader(headers, errors, fromRequest);
             return new ResponseEntity<String>(headers, HttpStatus.BAD_REQUEST);
         }
 
-        Property fromDB = properties.findOne(key);
-        if (fromDB != null) {
-            return new ResponseEntity<String>(fromDB.getValue(), HttpStatus.OK);
+        List<Property> fromDB = properties.findByKeyProductAndKeyComponentAndKeyNameOrderByRulePriorityDesc(key.getProduct(), key.getComponent(), key.getName());
+        for (Property each : fromDB) {
+            if (isMatch(instance, each)) {
+                addRuleHeader(headers, each);
+                return new ResponseEntity<String>(each.getValue(), headers, HttpStatus.OK);
+            }
         }
         return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
     }
 
-    private HttpHeaders getErrorHeader(List<String> errors, Property fromRequest) {
-        HttpHeaders headers = new HttpHeaders();
+    private boolean isMatch(String instance, Property each) {
         try {
-            headers.add("X-ReConf-Result", mapper.writeValueAsString(new PropertyResult(fromRequest, errors)));
+            ScriptEngineManager factory = new ScriptEngineManager();
+            ScriptEngine engine = factory.getEngineByName("JavaScript");
+            engine.put("regexp", each.getRuleRegexp());
+            engine.put("instance", instance);
+            engine.eval(JS);
+            return (boolean) engine.get("result");
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    private HttpHeaders addErrorHeader(HttpHeaders headers, List<String> errors, Property fromRequest) {
+
+        try {
+            headers.add(ReConfConstants.H_RESPONSE_RESULT, mapper.writeValueAsString(new PropertyResult(fromRequest, errors)));
+        } catch (Exception ignored) {
+        }
+        return headers;
+    }
+
+    private HttpHeaders addRuleHeader(HttpHeaders headers, Property property) {
+
+        try {
+            headers.add(ReConfConstants.H_RESPONSE_RULE, mapper.writeValueAsString(new Rule(property)));
         } catch (Exception ignored) {
         }
         return headers;
---
+++ src/test/resources/get-property1-ruled.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -G -v 'http://localhost:8080/product1/component1/simple_property1?instance=ci-123456'
