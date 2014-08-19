131ee2a58301fbd31ed6003c39c5a4a5731df719
Danilo Mutti <dmutti@gmail.com>
2014-08-19 11:26:10 -0300

    spring-security

---
+++ src/main/java/reconf/server/ApplicationSecurity.java
@@ -0,0 +1,50 @@
+/*
+ *    Copyright 2013-2014 ReConf Team
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package reconf.server;
+
+import javax.sql.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.http.*;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired DataSource dataSource;
+
+    @Value("${reconf.user.name}") String rootUserName;
+    @Value("${reconf.user.password}") String rootUserPassword;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/crud/**").hasRole("ROOT").and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("SELECT username, password,enabled FROM users WHERE username=?")
+            .authoritiesByUsernameQuery("SELECT username, authority FROM authorities WHERE username = ?")
+            .withUser(rootUserName).password(rootUserPassword).roles("ROOT");
+    }
+}
