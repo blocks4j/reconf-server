327246d05a36c54b002611083afe40f1d8f08437
Danilo Mutti <dmutti@gmail.com>
2014-08-21 21:59:41 -0300

    Update V001__initial.sql
    removing the version from the table name

--- src/main/resources/sql/common/V001__initial.sql
+++ src/main/resources/sql/common/V001__initial.sql
@@ -12,7 +12,7 @@ create table authorities (
     references users (username)
 );
 
-create table reconf_property_v1 (
+create table reconf_property (
     component_name varchar(256) not null,
     property_name varchar(256) not null,
     product_name varchar(256) not null,
@@ -20,25 +20,25 @@ create table reconf_property_v1 (
     property_desc varchar(4096),
     rule_priority integer not null,
     rule_regexp varchar(256) not null,
-    constraint pkPropertyV1 primary key (component_name, property_name, product_name, rule_name)
+    constraint pkProperty primary key (component_name, property_name, product_name, rule_name)
 );
 
-create table reconf_component_v1 (
+create table reconf_component (
     component_name varchar(256) not null,
     product_name varchar(256) not null,
     component_desc varchar(4096),
-    constraint pkComponentV1 primary key (component_name, product_name)
+    constraint pkComponent primary key (component_name, product_name)
 );
 
-create table reconf_product_v1 (
+create table reconf_product (
     product_name varchar(256) not null,
     product_desc varchar(4096),
-    constraint pkProductV1 primary key (product_name)
+    constraint pkProduct primary key (product_name)
 );
 
-create table user_product_v1 (
+create table user_product (
     product_name varchar(256) not null,
     username varchar(50) not null,
     insertion_date timestamp,
-    constraint pkUserProductV1 primary key (product_name, username)
+    constraint pkUserProduct primary key (product_name, username)
 );
